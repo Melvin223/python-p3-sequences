f67d6a0497af6083d13f6be29f9e0b003439ca47
Kymaany <melvinkimani81@gmail.com>
2024-05-26 22:39:50 +0300

    Sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,15 @@
 
 def print_fibonacci(length):
     pass
+    if length <= 0:
+        print([])  # No elements if length is less than or equal to 0
+        return
+
+    fibonacci_sequence = []
+    a, b = 0, 1
+
+    for _ in range(length):
+        fibonacci_sequence.append(a)
+        a, b = b, a + b
+
+    print(fibonacci_sequence)
